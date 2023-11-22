614c89ba42bac9bcff89bbde45b6e542944a92cd
Alexandre José <alexandrejose2527@gmail.com>
2023-11-22 17:49:25 -0300

    confirmado

--- aula 10 - funções/funcoes.js
+++ aula 10 - funções/funcoes.js
@@ -1,3 +1,4 @@
+//retornos valorA E valorB
 function soma(a, b) {
   let resultado = a + b;
   alert(`A soma de ${a} + ${b} = ${resultado}`);
@@ -53,3 +54,28 @@ const dadosDoUsuario = {
   endereco: "",
 };
 parametrosDoJeitoCerto(dadosDoUsuario);
+
+//RETORNOS com return
+function calcularMedia(a, b) {
+  const media = (a + b) / 2;
+  return media;
+}
+const resultado = calcularMedia(7, 2);
+console.log(resultado);
+
+function criarProduto(nome, preco) {
+  const produto = {
+    nome,
+    preco,
+    estoque: 1,
+  };
+  return produto;
+}
+
+const notebook = criarProduto("Notebook Intel Core i3 8GB", 2500);
+console.log(notebook);
+
+function areaRetangulo(base, altura) {
+  return base * altura;
+}
+console.log(areaRetangulo);
